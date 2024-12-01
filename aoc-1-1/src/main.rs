3727ca9bacd1aeed5ec532c202d1eab01e7a49cb
wbbraam <wbbraam@yahoo.com>
2024-12-01 11:52:52 +0100

    Added comments to day 1 source code

--- aoc-1-1/src/main.rs
+++ aoc-1-1/src/main.rs
@@ -13,12 +13,13 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
 
 
 fn main() {
-
+    //Allocate what is needed to complete and read the file.
     let all_lines = lines_from_file("/Users/ES94CO/Developer/rust/Advent/aoc-1-1/src/input1.txt");
     let mut total: i32 = 0;
     let mut list1: Vec<i32> = vec![];
     let mut list2: Vec<i32> = vec![];
 
+    //Preprocessing, parsing the lines into two lists
     for line in all_lines {
         println!("{:?}", line);
         //split line into two strings
@@ -28,12 +29,15 @@ fn main() {
         list1.push(first.parse::<i32>().unwrap());
         list2.push(second.parse::<i32>().unwrap());
     }
-
+    
+    //So we need to work on sorted lists. lets sort these vectors
     list1.sort();
     list2.sort();
 
+    //Iterate over both vectors at once. And calculate the difference between the values
     for (l1, l2) in list1.iter().zip(list2.iter()){
         let diff = l1 - l2;
+        // Abs for absolute value
         total = total + diff.abs();
     }
 
--- aoc-1-2/src/main.rs
+++ aoc-1-2/src/main.rs
@@ -15,6 +15,7 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
 // list is passed as a reference to avoid moving it (fix after error during compilation, LEARN!!!)
 fn count_occurrences(list: &Vec<i32>, value: i32) -> i32 {
     let mut count = 0;
+    // If I have the time, change this to functional programming instead of iterating.
     for item in list.iter() {
         if *item == value {
             count = count + 1;
@@ -24,27 +25,27 @@ fn count_occurrences(list: &Vec<i32>, value: i32) -> i32 {
 }
 
 fn main() {
-
+    //Allocate what is needed to complete and read the file.
     let all_lines = lines_from_file("/Users/ES94CO/Developer/rust/Advent/aoc-1-1/src/input1.txt");
     let mut total: i32 = 0;
     let mut list1: Vec<i32> = vec![];
     let mut list2: Vec<i32> = vec![];
 
+    //Preprocessing, parsing the lines into two lists
     for line in all_lines {
         println!("{:?}", line);
         //split line into two strings
-        let mut split = line.split("   ");
+        let mut split = line.split("   "); //There has to be a better way!
         let first = split.next().unwrap();
         let second = split.next().unwrap();
         list1.push(first.parse::<i32>().unwrap());
         list2.push(second.parse::<i32>().unwrap());
     }
-
-    list1.sort();
-    list2.sort();
-
+    
+    //This time we iterate over te left list and find the amount of occurences in the other list
     for item in list1.iter(){
-        let mut simscore = item * count_occurrences(&list2, *item);
+        // Took out occurence counting to a function, to keep it readable (and reusable later on :D)
+        let mut simscore = item * count_occurrences(&list2, *item); //Pass as reference else it fails horribly
         total = total + simscore
     }
 
